1c07849b0824dc29168f13c6906609e9c50cfb7a
Brian Strauch <bstrauch@confluent.io>
2021-09-22 16:21:34 -0700

    initial commit

---
+++ content.js
@@ -0,0 +1,27 @@
+// TODO: Watch menu items more efficiently
+
+let isMenuPage = false;
+
+let wait = function() {
+  setTimeout(function() {
+    let items = $('div[id^="menu-item-"]');
+    if (items.length === 0) {
+      isMenuPage = false;
+    } else if (!isMenuPage) {
+      isMenuPage = true;
+      items.each(hideIfTooExpensive);
+    }
+    wait();
+  }, 1000);
+}
+wait();
+
+function hideIfTooExpensive() {
+  let priceElem = $(this).find("strong").eq(0);
+  let priceText = priceElem.text().trim().substring(1);
+
+  if (Number(priceText) > 12.50) {
+    $(this).hide();
+  }
+}
+
